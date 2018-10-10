8a6d7fac64b30a900d7033293d78b74492c63f62
Richard Chen <richard.c.chen@gmail.com>
2018-10-10 16:34:23 -0400

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,12 @@
 // Code ControlledInput Component Here
+handleFirstNameChange = event => {
+  this.setState({
+    firstName: event.target.value
+  })
+}
+
+handleLastNameChange = event => {
+  this.setState({
+    lastName: event.target.value
+  })
+}
